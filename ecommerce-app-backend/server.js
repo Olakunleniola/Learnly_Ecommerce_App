477272cedfbc0633c0b9f3efb83b2bf9b107828b
olakunle <devlakunle@gmail.com>
2024-08-09 13:44:49 +0100

    Updated the server cors option to allow request from frontend origin

--- ecommerce-app-backend/server.js
+++ ecommerce-app-backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:8080',
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
 
