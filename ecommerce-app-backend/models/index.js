ce73622f978cafa22cf62f2a86fa0dfbddc64162
olakunle <devlakunle@gmail.com>
2024-08-09 05:46:03 +0100

    updated the environment variables in the .env file to include JWT_SECRET

--- ecommerce-app-backend/models/index.js
+++ ecommerce-app-backend/models/index.js
@@ -4,6 +4,7 @@ const config = require('../config/config')[process.env.NODE_ENV || 'development'
 const sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     dialect: config.dialect,
+    logging: false
 });
 
 const db = {
